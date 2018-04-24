6cc2f805a6207a9cfd31d2541ef6d6e0745bbf9e
Berinder Erik János <36894565+berindererikjanos@users.noreply.github.com>
2018-04-24 11:08:03 +0200

    Update and rename UMI3AN-ex10.py to UMI3AN-ex9.py

---
+++ UMI3AN-ex9.py
@@ -0,0 +1,8 @@
+#Berinder Erik János UMI3AN Csütörtök 10:00 HB
+#EX9
+#The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime
+#factor of the number 600851475143
+
+def primefactors():
+
+
