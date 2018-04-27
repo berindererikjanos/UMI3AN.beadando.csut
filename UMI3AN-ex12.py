e1540642b477802dacb1fa0a7f5ebc0047ed0b2f
Berinder Erik János <36894565+berindererikjanos@users.noreply.github.com>
2018-04-27 11:17:48 +0200

    Update UMI3AN-ex12.py

--- UMI3AN-ex12.py
+++ UMI3AN-ex12.py
@@ -9,7 +9,7 @@ def primszam_e(n):
     if n==0 or n==1:
         return False
     for i in range(2,int(math.sqrt(n)+1)):
-        if n%i == 00:
+        if n%i == 0:
             return False
     return True
 
