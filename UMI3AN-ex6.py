d81220ab24edda26173399b0e869368570109c1f
Berinder Erik János <36894565+berindererikjanos@users.noreply.github.com>
2018-04-29 00:42:05 +0200

    Update UMI3AN-ex6.py

--- UMI3AN-ex6.py
+++ UMI3AN-ex6.py
@@ -1,5 +1,6 @@
 #Berinder Erik János UMI3AN Csütörtök 10:00 HB
 #EX6
 #Write a program that automatically converts English text to Morse code and vice versa.
+#Írj egy olyan programot, amely automatikusan átalakítja az angol szöveget Morse-kódra és fordítva.
 
 def morse_code():
