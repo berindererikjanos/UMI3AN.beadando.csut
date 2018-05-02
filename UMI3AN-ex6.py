55e985fc3142b75a205edf447ea9d0e9dfb8a53b
Berinder Erik János <36894565+berindererikjanos@users.noreply.github.com>
2018-05-02 22:28:25 +0200

    Update UMI3AN-ex6.py

--- UMI3AN-ex6.py
+++ UMI3AN-ex6.py
@@ -4,5 +4,10 @@
 #Írj egy olyan programot, amely automatikusan átalakítja az angol szöveget Morse-kódra és fordítva.
 
 def morse_code():
-  Karakterek = {}
+  KARAKTEREK = ['A','B','C','D','E','F',
+        'G','H','I','J','K','L','M',
+        'N','O','P','Q','R','S','T',
+        'U','V','W','X','Y','Z',
+        '0','1','2','3','4','5',
+        '6',  '7',  '8','9']
   
