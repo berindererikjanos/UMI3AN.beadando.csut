d0e428580d183c7fbe51055d3d2a5617c4ee6dc6
Berinder Erik János <36894565+berindererikjanos@users.noreply.github.com>
2018-05-04 16:45:22 +0200

    Update UMI3AN-ex6.py

--- UMI3AN-ex6.py
+++ UMI3AN-ex6.py
@@ -3,7 +3,25 @@
 #Write a program that automatically converts English text to Morse code and vice versa.
 #Írj egy olyan programot, amely automatikusan átalakítja az angol szöveget Morse-kódra és fordítva.
 
-def morse_code():
+morze_kod =    {'A': '.- ', 'B': '-... ', 'C': '-.-. ',
+    'D': '-.. ', 'E': '. ', 'F': '..-. ',
+    'G': '--. ', 'H': '.... ', 'I': '.. ',
+    'J': '.--- ', 'K': '-.- ', 'L': '.-.. ',
+    'M': '-- ', 'N': '-. ', 'O': '--- ',
+    'P': '.--. ', 'Q': '--.- ', 'R': '.-. ',
+    'S': '... ', 'T': '- ', 'U': '..- ',
+    'V': '...- ', 'W': '.-- ', 'X': '-..- ',
+    'Y': '-.-- ', 'Z': '--.. ',
+
+    '0': '----- ', '1': '.---- ', '2': '..--- ',
+    '3': '...-- ', '4': '....- ', '5': '..... ',
+    '6': '-.... ', '7': '--... ', '8': '---.. ',
+    '9': '----. ',
+
+    ' ': '....... '
+    }
+
+
   
 
   
