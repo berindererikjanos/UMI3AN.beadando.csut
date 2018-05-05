ba583039ee6eb7f5602ae8826a84d7d96e403512
Berinder Erik János <36894565+berindererikjanos@users.noreply.github.com>
2018-05-05 12:37:07 +0200

    Update UMI3AN-ex9.py

--- UMI3AN-ex9.py
+++ UMI3AN-ex9.py
@@ -10,6 +10,11 @@ import math
 def prim_e(n):
     if n == 0 or n == 1:
         return False
+    for i in range(2,int(math.sqrt(n)+1)):
+        if n%i == 0:
+            return False
+    return True
+
     
     
 
