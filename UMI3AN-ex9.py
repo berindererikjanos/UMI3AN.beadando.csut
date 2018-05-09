de1df88c5b6cede25c1be9d4b29bc2c82eba92b9
Berinder Erik János <36894565+berindererikjanos@users.noreply.github.com>
2018-05-09 21:39:05 +0200

    Update UMI3AN-ex9.py

--- UMI3AN-ex9.py
+++ UMI3AN-ex9.py
@@ -34,3 +34,29 @@ print("A program sikeresen lefutott!")
 print("----------------------------------------")
 
 #Beadandófeladatvége
+
+#PÉLDA: n = 60
+#i megy 2-től gyök(60): azaz i [2,7] közt fog menni
+#1. lépés: i = 2
+#   while i prím és n%2 == 0
+#   2 prím és 60%2 == 0 ? -> igen :
+#       tenyezok = [2]
+#       60 = 60 // 2 = 30 ebben a lépésben n = 30
+#   2 prím és 30%2 == 0 ? -> igen
+#         tenyezok = [2,2]
+#         30 = 30 // 2 = 15 ebben a lépésben n = 15
+#     2 prím és 15%2==0 ? -> nem
+# #2. lépés: i = 3
+#     while i prím és n%3==0:
+#     3 prím és 15%3 == 0? -> igen:
+#         tenyezok = [2,2,3]
+#         15 = 15 // 3 = 5 ebben a lépésben már n = 5
+#     3 prím és 5%3 == 0 ? -> nem
+# 3. lépés: i = 4
+#     while i prím és n%4 == 0
+#     4 prím és 5%4==0? -#nem
+# 4. lépés: i = 5
+#     while i prím és n%4 == 0
+#     5 prím és 5%5 == 0 -> igen
+#         tenyezok = [2,2,3,5]
+#         5 = 5 // 5 = 1 ebben a lépésben n = 1
